a5403e849d281e738e6dcd126706cf4973d1a1b0
Gero Zayas <gerozayas@gmail.com>
2026-03-11 14:47:40 +0100

    feat: small program

--- main.c
+++ main.c
@@ -3,25 +3,17 @@
 #include "stdio.h"
 #include "string.h"
 #include "stb_ds.h"
+#include "stdlib.h"
 
 int main(void)
 {
-    int *array = NULL;
-    arrput(array, 44);
-    arrput(array, 56);
-    arrput(array, 34);
-    arrput(array, 23);
-    arrput(array, 29);
-    printf("The length of the array is  %ld\n", arrlen(array));
+    int c;
+    printf("Enter a character:\n");
+    c = getchar();
 
-    for (int elem = 0; elem < arrlen(array); elem++)
+    while (c != EOF)
     {
-        if (array[elem] == 23)
-        {
-            printf("ELEMENT IS 23!\n");
-            continue;
-        };
-
-        printf("%d\n", array[elem]);
-    };
+        putchar(c);
+        c = getchar();
+    }
 }
