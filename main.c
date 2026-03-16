7fe4672483a39b36017caac9ed01a48ea5fbad5b
Gero Zayas <gerozayas@gmail.com>
2026-03-16 22:48:09 +0100

    feat: add small man min val program

--- main.c
+++ main.c
@@ -1,14 +1,47 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-// print('Enter line')
-// line = input()
-// print('Line:', line)
+// maxval = None
+// minval = None
+// while True:
+//     line = input()
+//     line = line.strip()
+//     if line == "done" : break
+//     ival = int(line)
+//     if ( maxval is None or ival > maxval) :
+//         maxval = ival
+//     if ( minval is None or ival < minval) :
+//         minval = ival
+
+// print('Maximum', maxval)
+// print('Minimum', minval)
 
 int main(void)
 {
-    printf("Enter line\n");
+    char line[1000];
+    // int maxval, minval = 0;
 
-    char line[256];
-    fgets(line, sizeof(line), stdin);
-    printf("Line %s\n", line);
+    int maxval = 0;
+    int minval = 0;
+
+    while (gets(line) != NULL)
+    {
+        line[strcspn(line, "\n")] = '\0';
+
+        if (strcmp(line, "done") == 0)
+            break;
+
+        int ival = atoi(line);
+        if (maxval == 0 || ival > maxval)
+        {
+            maxval = ival;
+        }
+        if (minval == 0 || ival < minval)
+        {
+            minval = ival;
+        }
+    }
+    printf("Maximum %d\n", maxval);
+    printf("Minimum %d\n", minval);
 }
