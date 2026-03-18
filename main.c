bdb7bf1a88a1bfd1b1db3466bfd9fc3dde288db0
Gero Zayas <gerozayas@gmail.com>
2026-03-18 23:14:32 +0100

    feat: new small program

--- main.c
+++ main.c
@@ -2,16 +2,21 @@
 #include <string.h>
 #include <stdlib.h>
 
-
-int main(void) /* count new lines in input */
+int main(void)
 {
-    int c, nl;
-    nl = 0;
-    int spaces = 0;
+    int c;
+    int count = 0;
     while ((c = getchar()) != EOF)
-        if (c == '\n')
-            ++nl;
-        else if (c==' ')
-            ++spaces;
-    printf("%d %d\n", spaces, nl);
+    {
+        if (c == ' ')
+            ++count;
+        else if (c != ' ')
+            count = 0;
+        if (count > 1)
+        {
+            continue;
+        }
+
+        putchar(c);
+    }
 }
